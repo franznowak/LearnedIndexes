fc87a90ad2cfd4f3fb89b405e86563515d9ffc04
franznowak <franz.g.nowak@gmail.com>
2019-01-10 15:33:01 +0000

    Bugfix: rounding error in prediction

--- index/array.py
+++ index/array.py
@@ -2,7 +2,6 @@ import util.search as searcher
 
 
 def array_predict(input_data, key):
-    prediction = int(input_data.size/input_data.n_keys) * key
+    prediction = int(input_data.size/input_data.n_keys * key)
     searcher.search(input_data, prediction, key)
     return input_data.get_access_count()
-
