c12a8e68f7009ac3fc661b8e306b2bfc23cd0a1a
franznowak <franz.g.nowak@gmail.com>
2019-01-17 22:51:12 +0000

    Add model function to naive_tf module

--- index/naive_learned_index.py
+++ index/naive_learned_index.py
@@ -21,10 +21,58 @@ def main():
                               sep=" ", skipinitialspace=True)
 
     dataset = raw_dataset.copy()
-    print(dataset.tail())
+
+    train_dataset = dataset.sample(frac=0.8, random_state=0)
+    test_dataset = dataset.drop(train_dataset.index)
+
+    sns.pairplot(train_dataset[["index", "key"]], diag_kind="kde")
+
+    train_labels = train_dataset.pop('index')
+    test_labels = test_dataset.pop('index')
+
+    # Normalise data
+    train_stats = train_dataset.describe()
+    train_stats = train_stats.transpose()
+    normed_train_data = norm(train_dataset, train_stats)
+    normed_test_data = norm(test_dataset, train_stats)
+
+    model = build_model(train_dataset)
+
+    print(model.summary())
+    pass
+
+    EPOCHS = 1000
+
+    history = model.fit(
+        normed_train_data, train_labels,
+        epochs=EPOCHS, validation_split=0.2, verbose=0,
+        callbacks=[PrintDot()])
 
 
+def norm(x, stats):
+  return (x - stats['mean']) / stats['std']
 
+
+def build_model(train_dataset):
+  model = keras.Sequential([
+    layers.Dense(32, activation=tf.nn.relu, input_shape=[len(train_dataset.keys())]),
+    layers.Dense(32, activation=tf.nn.relu),
+    layers.Dense(1)
+  ])
+
+  optimizer = tf.train.RMSPropOptimizer(0.001)
+
+  model.compile(loss='mse',
+                optimizer=optimizer,
+                metrics=['mae', 'mse'])
+  return model
+
+
+# Display training progress by printing a single dot for each completed epoch
+class PrintDot(keras.callbacks.Callback):
+  def on_epoch_end(self, epoch, logs):
+    if epoch % 100 == 0: print('')
+    print('.', end='')
 
 
 if __name__ == "__main__":
