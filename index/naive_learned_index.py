d9c704036a2b7e6401b7c80d53a4a1fdbafc06a8
franznowak <franz.g.nowak@gmail.com>
2019-01-28 21:30:48 +0000

    Tidy up and only use necessary parts (e.g. only training set) Add callback for early stopping.

--- index/naive_learned_index.py
+++ index/naive_learned_index.py
@@ -1,14 +1,12 @@
 from __future__ import absolute_import, division, print_function
 
-import pathlib
-import numpy as np
-
 import pandas as pd
-import seaborn as sns
 
 import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers
+
+import matplotlib.pyplot as plt
 
 
 def main():
@@ -19,32 +17,29 @@ def main():
                               na_values="?", comment='\t',
                               sep=",", skipinitialspace=True)
 
-    dataset = raw_dataset.copy()
-
-    train_dataset = dataset.sample(frac=0.8, random_state=0)
-    test_dataset = dataset.drop(train_dataset.index)
-
-    # sns.pairplot(train_dataset[["index", "key"]], diag_kind="kde")
+    train_dataset = raw_dataset.copy()
 
     train_labels = train_dataset.pop('index')
-    test_labels = test_dataset.pop('index')
 
     # Normalise data
     train_stats = train_dataset.describe()
     train_stats = train_stats.transpose()
     normed_train_data = norm(train_dataset, train_stats)
-    normed_test_data = norm(test_dataset, train_stats)
 
     model = build_model(train_dataset)
 
-    print(model.summary())
-
-    EPOCHS = 1000  # Change depending on convergence, and max
+    max_epochs = 1000 # Change depending on convergence, and max
+    callbacks = [keras.callbacks.EarlyStopping(monitor='mean_absolute_error',
+                                               min_delta=0, patience=2,
+                                               verbose=0, mode='auto',
+                                               baseline=None), PrintDot()]
 
     history = model.fit(
         normed_train_data, train_labels,
-        epochs=EPOCHS, validation_split=0.2, verbose=0,
-        callbacks=[PrintDot()]) # Different callback
+        epochs=max_epochs, validation_split=0.2, verbose=0,
+        callbacks=callbacks)
+
+    plot_history(history)
 
 
 def norm(x, stats):
@@ -67,11 +62,31 @@ def build_model(train_dataset):
     return model
 
 
+def plot_history(history):
+    hist = pd.DataFrame(history.history)
+    hist['epoch'] = history.epoch
+    plt.figure()
+    plt.xlabel('Epoch')
+    plt.ylabel('Mean Abs Error')
+    plt.plot(hist['epoch'], hist['mean_absolute_error'],
+             label='Train Error')
+    plt.legend()
+    plt.ylim([0, 50])
+    # plt.figure()
+    # plt.xlabel('Epoch')
+    # plt.ylabel('Mean Square Error')
+    # plt.plot(hist['epoch'], hist['mean_squared_error'],
+    #          label='Train Error')
+    # plt.legend()
+    # plt.ylim([0, 800])
+    plt.show()
+
+
 # Display training progress by printing a single dot for each completed epoch
 class PrintDot(keras.callbacks.Callback):
-   def on_epoch_end(self, epoch, logs):
-     if epoch % 100 == 0: print('')
-     print('.', end='')
+    def on_epoch_end(self, epoch, logs):
+        if epoch % 100 == 0: print('')
+        print('.', end='')
 
 
 if __name__ == "__main__":
