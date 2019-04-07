fdfb780a2176cefbeec08cb0897cedbe2e4b8091
franznowak <franz.g.nowak@gmail.com>
2019-04-07 17:01:06 +0100

    Use naive index class for recursive index. Rename, clean up.

---
+++ index/recursive_learned_index.py
@@ -0,0 +1,59 @@
+from .naive_learned_index import Model
+
+
+EPOCHS = 100
+WEIGHTS_PATH = "../data/indexes/recursive_learned_index"
+DATA_PATH = "../data/datasets"
+
+
+def train_recursive(stages, all_data, nn_complexity):
+    """
+    Hybrid end-to-end training algorithm as described in The Case For
+    Learned Index Structures, Kraska et al. p8.
+
+    :param stages:
+        list of widths of the regression tree for each level.
+    :param all_data:
+        list of all the data (key-index pairs) for the dataset.
+    :param  nn_complexity:
+        list of shapes of the NNs for all NN-model in the regression tree.
+        each shape is a list of widths of neurons for each layer.
+
+    :return trained_index
+
+    """
+    trained_index = []
+
+    M = len(stages)
+
+    tmp_records = []
+    for i in range(M):
+        tmp_records.append([])
+        for j in range(stages[i]):
+            tmp_records[i].append([])
+
+    tmp_records[0][0] = all_data
+
+    for i in range(M):
+        trained_index.append([])
+        for j in range(stages[i]):
+            model = Model(nn_complexity[i][j], tmp_records[i][j], "{}weights{}"
+                                                                  "_{}.h5"
+                          .format(WEIGHTS_PATH, i, j))
+            model.train()
+            trained_index[i].append(model)
+
+            if i < M:
+                for r in tmp_records[i][j]:
+                    p = int(trained_index[i][j].predict(r.key)/stages[i+1])
+                    tmp_records[i+1][p].add(r)
+
+    return trained_index
+
+
+# if __name__ == "__main__":
+#     # TODO: Make Tests
+#     for i in range(config.N_RUNS):
+#         for j in range(config.N_INTERPOLATIONS):
+#             index = train_recursive([1, 2, 3], '{}/run{}inter{}'.format(
+#                 DATA_PATH, i, j))
