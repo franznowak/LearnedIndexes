6ea18e10d90c6501d94d6a8df673ca233f341ce2
franznowak <franz.g.nowak@gmail.com>
2019-05-17 13:08:43 +0100

    Add tests and finish hybrid index

--- index/hybrid_learned_index.py
+++ index/hybrid_learned_index.py
@@ -1,61 +1,139 @@
 # --------------------------------------------------------------------
-# hybrid_learned_index.py - function outlining training of a hybrid learned
-# index.
+# hybrid_learned_index.py - class for making predictions using index
 # December 2018 - May 2019 Franz Nowak
 # --------------------------------------------------------------------
 
-from .naive_learned_index import Model
+import time
+
+from index.btree_index import BTreeSet
+from index.naive_learned_index import Model
+import pandas as pd
+import logging
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
 
 
-EPOCHS = 100
-WEIGHTS_PATH = "../data/indexes/recursive_learned_index"
-DATA_PATH = "../data/datasets"
-
-
-def train_recursive(stages, all_data, nn_complexity, threshold:int):
+class RecursiveLearnedIndex:
     """
-    Hybrid end-to-end training algorithm as described in The Case For
-    Learned Index Structures, Kraska et al. p8.
-
-    :param stages:
-        list of widths of the regression tree for each level.
-    :param all_data:
-        list of all the data (key-index pairs) for the dataset.
-    :param  nn_complexity:
-        list of shapes of the NNs for all NN-model in the regression tree.
-        each shape is a list of widths of neurons for each layer.
-    :param threshold:
-        max absolute error of NN before other index is used.
-
-    :return trained_index
+    A regression tree of neural networks that takes a one dimensional input
+    and returns a prediction of the position in the dataset by iteratively
+    predicting and thus finding the most accurate model.
 
     """
-    trained_index = []
+    def __init__(self, stages, nn_complexity, threshold):
+        """
+        Initialises the multilevel learned index with B-tree support.
 
-    M = len(stages)
+        :param stages:
+            list of widths of the regression tree for each level.
+        :param  nn_complexity:
+            list of shapes of the NNs for all NN-model in the regression tree.
+            each shape is a list of widths of neurons for each layer.
+        :param threshold:
+            highest permitted prediction error before switching to B-tree.
 
-    tmp_records = []
-    for i in range(M):
-        tmp_records.append([])
-        for j in range(stages[i]):
-            tmp_records[i].append([])
+        :return trained_index
 
-    tmp_records[0][0] = all_data
+        """
+        self.stages = stages
+        self.nn_complexity = nn_complexity
+        self.data_size = 0  # the total number of true indexes
+        self.index = None
+        self.threshold = threshold
 
-    for i in range(M):
-        trained_index.append([])
-        for j in range(stages[i]):
-            model = Model(nn_complexity[i][j], tmp_records[i][j], "{}weights{}"
-                                                                  "_{}.h5"
-                          .format(WEIGHTS_PATH, i, j))
-            model.train()
-            trained_index[i].append(model)
+    def train(self, all_data, weights_path='', graph_path='', epochs=100):
+        """
+        Hybrid end-to-end training algorithm as described in The Case For
+        Learned Index Structures, Kraska et al.
 
-            if i < M:
-                for r in tmp_records[i][j]:
-                    p = int(trained_index[i][j].predict(r.key)/stages[i+1])
-                    tmp_records[i+1][p].add(r)
+        :param all_data:
+            dataframe of all the data (key-index pairs) of the dataset.
+        :param weights_path:
+            path where model weights shall be stored.
+        :param graph_path:
+            path where training history graphs shall be stored.
+        :param epochs:
+            number of epochs for the gradient descent of the NN-training.
 
-    # TODO: if error too high (above threshold) replace with b-tree here
+        :return trained_index
 
-    return trained_index
+        """
+        trained_index = []
+
+        n_stages = len(self.stages)
+        self.data_size = len(all_data.index)
+
+        tmp_records = []
+        for i in range(n_stages):
+            tmp_records.append([])
+            for j in range(self.stages[i]):
+                df = pd.DataFrame([], columns=['key', 'index'])
+                tmp_records[i].append(df)
+
+        tmp_records[0][0] = all_data
+
+        for i in range(n_stages):
+            trained_index.append([])
+            for j in range(self.stages[i]):
+                model = Model(self.nn_complexity[i],
+                              tmp_records[i][j])
+                model.train(weights_path, "weights{}_{}.h5"
+                            .format(i, j), epochs)
+                model.plot_history(graph_path, '{}_history_{}_{}.png'
+                                   .format(int(time.time()), i, j))
+
+                # if error is too high, replace with b_tree
+                if model.model.evaluate(tmp_records[i][j].loc[:, 'key'],
+                                        tmp_records[i][j].loc[:, 'index']) < \
+                        self.threshold:
+                    trained_index[i].append(model)
+                else:
+                    btree = BTreeSet(4)
+                    for i in range(len(tmp_records[i][j])):
+                        btree.add(tmp_records[i][j].loc[i, :])
+                    trained_index[i].append(btree)
+
+                if i < n_stages-1:
+                    rs = tmp_records[i][j]
+
+                    for k in rs.index:
+                        r = rs.loc[k, :]
+                        p = trained_index[i][j].predict(rs.at[k, 'key'])
+                        q = self._get_next_model_index(i, p)
+                        tmp_records[i+1][q] = tmp_records[i+1][q].append(
+                            r.astype('int64'))
+
+                print("model[{}][{}]:{} entries"
+                      .format(i, j, len(tmp_records[i][j].index)))
+
+        self.index = trained_index
+
+    def _get_next_model_index(self, current_stage, current_prediction):
+        """
+        Returns the index of the next model in the next stage of the tree to
+        be used for prediction.
+
+        :param current_stage: the current stage at which predictions are made
+        :param current_prediction: the last prediction made by the current model
+        :return: index of the next model down the tree
+
+        """
+        p = int(current_prediction * self.stages[current_stage + 1] /
+                self.data_size)
+        return min(self.stages[current_stage + 1] - 1, max(0, p))
+
+    def predict(self, key):
+        """
+        Predicts the position of the data based on the key.
+
+        :param key: int key input to the model
+        :return: prediction of position
+
+        """
+        j = 0
+        n_stages = len(self.stages)
+        for i in range(n_stages - 1):
+            j = self._get_next_model_index(i, self.index[i][j].predict(key))
+
+        return self.index[n_stages-1][j].predict(key)
